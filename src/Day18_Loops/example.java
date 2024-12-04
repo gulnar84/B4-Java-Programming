4d7637204b5d2501d4bff859004ed3ba9d30a261
gulnar84 <gulnarresearcher@gmail.com>
2024-12-04 11:56:49 -0500

    new comment

---
+++ src/Day18_Loops/example.java
@@ -0,0 +1,65 @@
+package Day18_Loops;
+
+public class example {
+    public static void main(String[] args) {
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print(j + " ");
+//            }
+//            System.out.println(); //program 1
+
+
+
+//        for (int i = 0; i <= 4; i++) {
+//            for (int j = 1; j <= i+1; j++) {
+//                System.out.print(j + " ");
+//            }
+//            System.out.println();
+
+//        for (int i = 0; i < 6; i++) {
+//            for (int j = 1; j < 6-i; j++) {
+//                System.out.print(j);
+//            }
+//            System.out.println(); // program 3
+//        int number=1;
+//        for (int i = 1; i <= 4; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print(number +" ");
+//                number++;
+//            }
+//            System.out.println();         //program 2
+
+
+//          //String word="*";
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= 5-i; j++) {
+//
+//                System.out.print("*");
+//
+//            }
+//            System.out.println();         //program 2
+
+//        //String word="*";
+//        int number=5;
+//        for (int i = 1; i <= number; i++) {
+//            for (int j = 1; j <=number-i; j++) {
+//                System.out.print("*");
+//               // number++;
+//            }
+//            System.out.println();         //program 2
+
+
+        //int i,j;
+
+//           int n=1;
+//           n+=3;
+        for (int i = 1; i <=6; i++) {
+            for (int j =1; j<=i; j++) {
+
+                System.out.print("* ");
+            }
+            System.out.println();
+
+
+        }}}
+
